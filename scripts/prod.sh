76b9068369b0fb4e23254b8432abf61a2edf5ad2
Michael Rosenberg <michael@mrosenberg.pub>
2022-07-02 17:08:27 -0400

    Added --release flag to prod; WASM file is now 80% smaller

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pushd frontend
-RUSTFLAGS=--cfg=web_sys_unstable_apis trunk build --public-url /assets/
+RUSTFLAGS=--cfg=web_sys_unstable_apis trunk build --release --public-url /assets/
 popd
 
 pushd server

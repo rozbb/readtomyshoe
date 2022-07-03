ce37914e05fd2c11a4bff261e02ed7d4680b8b3a
Michael Rosenberg <michael@mrosenberg.pub>
2022-07-03 15:07:31 -0400

    Inlined and gzipped responses; website is no 96% smaller than last week

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,9 +2,12 @@ mod add_article;
 mod list_articles;
 mod tts;
 
-use std::future::ready;
-use std::net::{IpAddr, Ipv6Addr, SocketAddr};
-use std::str::FromStr;
+use std::{
+    future::ready,
+    net::{IpAddr, Ipv6Addr, SocketAddr},
+    path::PathBuf,
+    str::FromStr,
+};
 
 use axum::{
     http::{HeaderValue, StatusCode},
@@ -15,7 +18,10 @@ use axum::{
 use axum_extra::routing::SpaRouter;
 use clap::Parser;
 use tower::ServiceBuilder;
-use tower_http::{services::ServeDir, trace::TraceLayer};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -62,28 +68,31 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
+    // A generic error handler that just returns 500
+    let ret_500 = |_| ready(StatusCode::INTERNAL_SERVER_ERROR);
+
+    // Make a service that returns the static assets from /assets, and serves index.html
+    // everywhere else. Also try to serve gzipped assets when the .gz file exists
+    let static_dir: PathBuf = opt.static_dir.as_str().into();
+    let asset_service = get_service(ServeDir::new(&opt.static_dir).precompressed_gzip());
+    let index_service =
+        get_service(ServeFile::new(static_dir.join(&opt.index_file)).precompressed_gzip());
+    let asset_router = Router::new()
+        .nest("/assets", asset_service.handle_error(ret_500.clone()))
+        .layer(ServiceBuilder::new().map_response(add_asset_headers))
+        .fallback(index_service.handle_error(ret_500.clone()));
+
     // Make a service that just returns files from /audio_blobs
-    let audio_blob_service = get_service(ServeDir::new("audio_blobs"))
-        .handle_error(|_| ready(StatusCode::INTERNAL_SERVER_ERROR));
+    let audio_blob_service =
+        get_service(ServeDir::new("audio_blobs")).handle_error(ret_500.clone());
+    let app = asset_router.nest("/api/audio-blobs", audio_blob_service);
 
-    // Add a Service-Worker-Allowed header to the static assets. This allows a service worker from
-    // /assets/ to cache items from the root directory
-    let header_appender = ServiceBuilder::new().map_response(|mut resp: Response| {
-        let headers = resp.headers_mut();
-        headers.insert("Service-Worker-Allowed", HeaderValue::from_static("/"));
-        resp
-    });
-    let asset_router = SpaRouter::new("/assets", &opt.static_dir).index_file(&opt.index_file);
-    let asset_router = Router::from(asset_router).layer(header_appender);
-
-    let app = Router::new()
-        .route("/healthz", get(|| async { "ok" }))
-        .nest("/api/audio-blobs", audio_blob_service)
-        .merge(asset_router);
-
-    // Set up /api/list-articles
+    // Set up /api/
     let app = list_articles::setup(app, &opt.audio_blob_dir);
     let app = add_article::setup(app, &opt.audio_blob_dir);
+
+    // Make a /healthz endpoint for Docker health checks
+    let app = app.route("/healthz", get(|| async { "ok" }));
 
     // Tracing for the entire app
     let app = app.layer(TraceLayer::new_for_http());
@@ -98,4 +107,21 @@ async fn main() {
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .expect("Unable to start server");
+}
+
+/// Adds headers for ReadToMyShoe assets. Assets are cached by the service worker, and not the HTTP
+/// cache
+fn add_asset_headers(mut resp: Response) -> Response {
+    let headers = resp.headers_mut();
+
+    // Add a Service-Worker-Allowed header to the static assets. This allows a service worker from
+    // /assets/ to cache items from the root directory
+    headers.insert("Service-Worker-Allowed", HeaderValue::from_static("/"));
+
+    // Do not use the HTTP cache. The client's service worker does one level of caching
+    headers.insert(
+        "Cache-control",
+        HeaderValue::from_static("public, no-cache"),
+    );
+    resp
 }
